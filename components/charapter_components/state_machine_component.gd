0f1c2842ebd810985f0499343ba88ee96bd36de2
Alekonios <alekongames@gmail.com>
2024-12-04 23:50:04 +0300

    add animation tree

--- components/charapter_components/state_machine_component.gd
+++ components/charapter_components/state_machine_component.gd
@@ -12,16 +12,27 @@ var current_state = Idle
 
 func _process(delta: float) -> void:
 	update_states()
+	if Input.is_action_just_pressed("ui_accept"):
+		jump_anim.rpc()
 
 func update_states():
 	if !is_multiplayer_authority(): return
 	match current_state:
 		Idle:
-			#animator.set("parameters/Movement/transition_request", "idle_")
-			sync_animator.play("idle")
+			update_with_rpc.rpc("idle_")
 		Walk:
-			#animator.set("parameters/Movement/transition_request", "walk_")
-			sync_animator.play("walk")
+			update_with_rpc.rpc("walk_")
 		Run:
-			#animator.set("parameters/Movement/transition_request", "run_")
-			sync_animator.play("run")
+			update_with_rpc.rpc("run_")
+
+
+
+@rpc("any_peer", "call_local")
+func update_with_rpc(name_):
+	animator.set("parameters/Movement/transition_request", name_)
+	
+	
+@rpc("any_peer", "call_local")
+func jump_anim():
+	animator.set("parameters/JumpAnim/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
+	
